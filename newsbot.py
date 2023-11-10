434b31d35bd2fcc6093f3f151a9dd28bf936eeb7
constantrik <rickhdev@gmail.com>
2023-11-10 12:39:27 -0400

    Added base code for connection to the client.

--- newsbot.py
+++ newsbot.py
@@ -1,15 +1,18 @@
 import os
-import discord
 
+import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
     print(f'{client.user}has connected to Discord!')
+
 client.run(TOKEN)
